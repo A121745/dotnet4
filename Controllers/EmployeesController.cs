8cf6d8711634336152d459f3892a89d6c2ad9e71
A121745 <100848876+A121745@users.noreply.github.com>
2022-08-05 06:51:52 +0530

    Add files via upload

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,101 @@
+﻿using MVCwithDatabase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCwithDatabase.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET: Employees
+        public ActionResult Index()
+        {
+            List<Employee> list = Employee.GetEmployees();
+            return View(list);
+        }
+
+        // GET: Employees/Details/5
+        public ActionResult Details(int id = 0)
+        {
+            Employee e = Employee.GetSingleEmployee(id);
+            return View(e);
+        }
+
+        // GET: Employees/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employees/Create
+        [HttpPost]
+        public ActionResult Create(Employee obj)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                Employee.InsertEmployee(obj);
+               // ViewBag.message = "Insertion Sucessfull";
+               // return View();
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                //ViewBag.message = e.Message;
+                //return View();
+                return RedirectToAction("Index");
+            }
+        }
+
+        // GET: Employees/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Employee e = Employee.GetSingleEmployee(id);
+            return View(e);
+        }
+
+        // POST: Employees/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Employee e)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                Employee.UpdateEmployee(id, e);
+                return RedirectToAction("Index");
+            }
+            catch 
+            {
+                return View();
+            }
+        }
+
+        // GET: Employees/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Employee e = Employee.GetSingleEmployee(id);
+            return View(e);
+
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, Employee e=null)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                Employee.DeleteEmployee(id);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCwithDatabase.Models
+{
+    public class Employee
+    {
+
+        [Key]
+        public int EmpNo { get; set; }
+        
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Please enter name")]
+        [StringLength(10, ErrorMessage = "The {0} value cannot exceed {1} characters. ")]
+        public string Name { get; set; }
+
+        [Range(1000, 500000, ErrorMessage = "Please enter values between 1000-500000")]
+      //  [MaxLength(6), MinLength(4)]
+        [Display(Name = "Basic Salary")]
+        [DataType(DataType.Currency)]
+        public decimal Basic { get; set; }
+
+        public int DeptNo { get; set; }
+        public static List<Employee> GetEmployees() {
+           
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString= @"Data Source=(localdb)\ProjectsV13;Initial Catalog=JKJuly2022;Integrated Security=True;";
+            List<Employee> list = new List<Employee>();
+            try
+            {
+                cn.Open();
+                SqlCommand s = new SqlCommand();
+                s.Connection = cn;
+                s.CommandType = System.Data.CommandType.StoredProcedure;
+                s.CommandText = "DisplayEmployees1";
+                SqlDataReader dr = s.ExecuteReader();
+                while (dr.Read())
+                {
+                    Employee e = new Employee();
+                    e.EmpNo=(int)dr["EmpNo"];
+                    e.Name = (string)dr["Name"];
+                    e.Basic = (decimal)dr["Basic"];
+                    e.DeptNo = (int)dr["DeptNo"];
+                    list.Add(e);
+
+                }
+                dr.Close();
+
+            }
+            catch (Exception e1)
+            {
+                Console.WriteLine(e1.Message);
+            }
+
+            finally
+            {
+                cn.Close();
+            }
+            return list;
+        }
+
+        internal static void InsertEmployee(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Employee GetSingleEmployee(int EmpNo) {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=JKJuly2022;Integrated Security=True;";
+            Employee obj = null;
+
+            cn.Open();
+            SqlCommand s = new SqlCommand();
+            s.Connection = cn;
+            s.CommandType = System.Data.CommandType.StoredProcedure;
+            s.CommandText = "GetSingleEmployee";
+            s.Parameters.AddWithValue("@EmpNo", EmpNo);
+
+            SqlDataReader dr = s.ExecuteReader();
+
+            
+
+            if (dr.Read())
+            {
+                obj = new Employee();
+                obj.EmpNo = (int)dr["EmpNo"];
+                obj.Name = (string)dr["Name"];
+                obj.Basic = (decimal)dr["Basic"];
+                obj.DeptNo = (int)dr["DeptNo"];
+            }
+
+
+            cn.Close();
+            return obj;
+
+        }
+        public static void InsertEmployee(Employee obj)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=JKJuly2022;Integrated Security=True;";
+
+
+            cn.Open();
+            SqlCommand s = new SqlCommand();
+            s.Connection = cn;
+            s.CommandType = System.Data.CommandType.StoredProcedure;
+            s.CommandText = "InsertEmployee";
+            s.Parameters.AddWithValue("@EmpNo", obj.EmpNo);
+            s.Parameters.AddWithValue("@Name", obj.Name);
+            s.Parameters.AddWithValue("@Basic", obj.Basic);
+            s.Parameters.AddWithValue("@DeptNo", obj.DeptNo);
+            s.ExecuteNonQuery();
+            cn.Close();
+        }
+        public static void UpdateEmployee(int id, Employee obj)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=JKJuly2022;Integrated Security=True;";
+
+
+            cn.Open();
+            SqlCommand s = new SqlCommand();
+            s.Connection = cn;
+            s.CommandType = System.Data.CommandType.StoredProcedure;
+            s.CommandText = "UpdateEmployee";
+            s.Parameters.AddWithValue("@EmpNo", id);
+            s.Parameters.AddWithValue("@Name", obj.Name);
+            s.Parameters.AddWithValue("@Basic", obj.Basic);
+            s.Parameters.AddWithValue("@DeptNo", obj.DeptNo);
+            s.ExecuteNonQuery();
+        }
+        public static void DeleteEmployee(int obj)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=JKJuly2022;Integrated Security=True;";
+
+
+            cn.Open();
+            SqlCommand s = new SqlCommand();
+            s.Connection = cn;
+            s.CommandType = System.Data.CommandType.StoredProcedure;
+            s.CommandText = "DeleteEmployee";
+            s.Parameters.AddWithValue("@EmpNo", obj);
+            s.ExecuteNonQuery();
+            cn.Close();
+        }
+
+    }
+}
